c629105784dadcb36cf240b1fe3321f30f4c4e09
SherwoodArcher <rinaldobneto007@gmail.com>
2018-06-06 19:36:18 -0300

    Correção Categoria

--- application/views/category.php
+++ application/views/category.php
@@ -19,7 +19,7 @@
     <!-- Fim Titulo da seção -->
 <?php
 
-    $a =0;
+    $a = 0;
 
     foreach($query as $categoryItem){
         if($a == 0) {
@@ -28,11 +28,11 @@
         echo "  <div class='col-xs-12 col-sm-6 col-md-4'>
                     <div class='thumbnail'>
                         <img src='".$categoryItem['post_photos']."' alt='Teatro Municipal'>
-                        <div class='corCultural' style='height: 5px'></div>
+                        <div class='cor".$category_class."' style='height: 5px'></div>
                         <div class='caption'>
                             <h3>".$categoryItem['post_title']."</h3>
                             <p>".substr($categoryItem['post_description'], 0, 300)." ...</p>
-                            <p><a href='".base_url('post/'.$categoryItem['post_id'])."' class=''>Mais detalhes</a></p>
+                            <p><a href='".base_url('index.php/post/'.$categoryItem['post_id'])."' class=''>Mais detalhes</a></p>
                         </div>
                     </div>
                 </div>";
