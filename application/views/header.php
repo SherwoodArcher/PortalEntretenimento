fc7db500ea7cc22f0cf8d01a58f45b9678b65b6f
SherwoodArcher <rinaldobneto007@gmail.com>
2018-06-06 19:13:59 -0300

    Correção Header

--- application/views/header.php
+++ application/views/header.php
@@ -83,12 +83,12 @@
                 <div class="collapse navbar-collapse" id="BarraDeNavegacao">
                     <!-- Categorias da Barra de Navegação -->
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="<?php echo base_url('categoria/partiurole');?>">Partiu Rolê</a></li>
-                        <li><a href="<?php echo base_url('categoria/descontracao');?>">Descontração</a></li>
-                        <li><a href="<?php echo base_url('categoria/emmovimento');?>">Em Movimento</a></li>
-                        <li><a href="<?php echo base_url('categoria/nafaixa');?>">Na Faixa</a></li>
-                        <li><a href="<?php echo base_url('categoria/geek');?>">Geek</a></li>
-                        <li><a href="<?php echo base_url('categoria/cultural');?>">Cultural</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/partiurole');?>">Partiu Rolê</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/descontracao');?>">Descontração</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/emmovimento');?>">Em Movimento</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/nafaixa');?>">Na Faixa</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/geek');?>">Geek</a></li>
+                        <li><a href="<?php echo base_url('index.php/categoria/cultural');?>">Cultural</a></li>
                         <li class="mobile-hide"><a href="#login"><span class="glyphicon glyphicon-user"></span></a></li>                    
                         <li class="mobile-hide"><a href="#search"><span class="glyphicon glyphicon-search"></span></a></li>
                     </ul>
