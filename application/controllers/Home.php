ec213050d8c05997cd9acbf0fd988c80430b48cf
SherwoodArcher <rinaldobneto007@gmail.com>
2018-06-19 19:54:36 -0300

    Teste

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,7 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('home');
+		$data['title'] = "Boom - Portal de Entretenimento"; 
+		$this->load->view('home',$data);
 	}
 }
