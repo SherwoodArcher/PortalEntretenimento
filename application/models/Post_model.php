daf3296631cfe4164094f448cae8512af9f6df2e
SherwoodArcher <rinaldobneto007@gmail.com>
2018-06-19 21:45:36 -0300

    Testes

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -6,12 +6,12 @@ class Post_model extends CI_Model {
     public function insert()
     {
         $data = array(
-            'post_title' => $this->input->post('nome'),
+            'post_title' => $this->input->post('titulo'),
             'post_user_id' => $this->session->user_id,  
             'post_date_time' => date("Y-m-d"),  
             'post_photos' => $this->input->post('foto'),  
             'post_description' => $this->input->post('descricao'),  
-            'post_category' => $this->input->post('category')         
+            'post_category' => $this->input->post('categoria')         
         );       
         return $this->db->insert('Post', $data);
     }
